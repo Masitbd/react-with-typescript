dcacc9b1df395d6ad4499e6e70e585127e916e29
Murad Sarker <sarker24@gmail.com>
2021-05-30 15:58:15 +0200

    Work on onchange field function

--- src/AddToList.tsx
+++ src/AddToList.tsx
@@ -9,28 +9,47 @@ const AddToList = () =>{
         img: ""
 
     })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+
+        })
+
+    }
     return(
         <div className= "AddToList">
             <input type="text"
             placeholder="Name"
             className="AddToList-input"
             value= {input.name}
+            onChange={handleChange}
+            name= "name"
             />
               <input type="text"
             placeholder="age"
             className="AddToList-input"
+            onChange={handleChange}
             value= {input.age}
+            name= "age"
             />
               <input type="text"
             placeholder="image Url"
             className="AddToList-input"
+            onChange={handleChange}
             value= {input.img}
+            name= "img"
             />
               <input type="text"
             placeholder="note"
             className="AddToList-input"
+            onChange={handleChange}
             value= {input.note}
+            name= "note"
             />
+            <button>Add to list</button>
         </div>
     )
 }
